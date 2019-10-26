e9eeabeaed42b41fa4cd265c0528ead968fcc3e4
dcgoodwin2112 <dcgoodwin2112@gmail.com>
2019-10-25 21:07:08 -0400

    Update contact page layout.

--- src/pages-markdown/contact.md
+++ src/pages-markdown/contact.md
@@ -2,4 +2,4 @@
 title: "Contact"
 ---
 
-Have a question, comment, or maybe a project idea? [Twitter](https://twitter.com/dcgoodwin2112) and [LinkedIn](https://www.linkedin.com/in/dcgoodwin2112/) are usually the best places to reach me, but you can also send me a message using the contact form below. I'll reply back as soon as I can.
+Have a question, comment, or maybe a project idea? [Twitter](https://twitter.com/dcgoodwin2112) and [LinkedIn](https://www.linkedin.com/in/dcgoodwin2112/) are usually the best places to reach me, but you can also send me a message using the contact form here. I'll reply back as soon as I can.
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -26,43 +26,46 @@ const Contact = ({ data }) => {
     <Layout>
       <SEO title="Contact" />
       <h1>{frontmatter.title}</h1>
-      <div
-        className="contact-page"
-        dangerouslySetInnerHTML={{ __html: html }}
-      />
       {confirm !== "" && <div className="confirm-message">{confirm}</div>}
-      <form
-        name="contact"
-        className="contact-form"
-        method="POST"
-        data-netlify="true"
-        netlify-honeypot="name-other"
-        onSubmit={handleSubmit}
-      >
-        <label>
-          Subject:
-          <br />
-          <input
-            type="text"
-            name="subject"
-            value={values.subject}
-            onChange={handleChange}
-          />
-        </label>
-        <label style={{ display: "none" }}>
-          Name: <input name="name-other" />
-        </label>
-        <label>
-          Message:
-          <br />
-          <textarea
-            name="message"
-            value={values.message}
-            onChange={handleChange}
-          />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <div className="contact-form-container">
+        <div
+          className="contact-page"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+
+        <form
+          name="contact"
+          className="contact-form"
+          method="POST"
+          data-netlify="true"
+          netlify-honeypot="lastname"
+          onSubmit={handleSubmit}
+        >
+          <label>
+            Subject:
+            <br />
+            <input
+              type="text"
+              name="subject"
+              value={values.subject}
+              onChange={handleChange}
+            />
+          </label>
+          <label style={{ display: "none" }}>
+            Name: <input name="name-other" />
+          </label>
+          <label>
+            Message:
+            <br />
+            <textarea
+              name="message"
+              value={values.message}
+              onChange={handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
     </Layout>
   )
 }
